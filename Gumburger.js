7001ebcf55c4a4ef2ab61d6a95e3b227ee6f88d4
Daeyr0ch1a <157944146+Daeyr0ch1a@users.noreply.github.com>
2024-02-02 11:54:32 +0600

    Add files via upload

---
+++ Gumburger.js
@@ -0,0 +1,24 @@
+const burgerIcon = document.querySelector('.burger-icons')
+const closeIcon = document.querySelector(".close-icons")
+const menu = document.querySelector(".menu")
+
+console.log(burgerIcon)
+console.log(closeIcon)
+console.log(menu)
+
+burgerIcon.addEventListener("click",function(){
+    menu.style.right="0"
+})
+closeIcon.addEventListener("click",function(){
+    menu.style.right="-100%"
+})
+document.addEventListener("click",function(event){
+if(!menu.contains(event.target)&& event.target !== burgerIcon )
+menu.style.right="-100%"
+})
+window.addEventListener("resize",function(){
+    if(window.innerWidth >768){
+        menu.style.right="-100%"
+    }
+    
+})
--- Lox.js
+++ Lox.js
@@ -1,1 +1,33 @@
+const images  = document.querySelectorAll('.conte .loshara .loshra img');
+const loshara = document.querySelector('.loshara');
+let count = 0;
+let width;
 
+function init() {
+    width = document.querySelector('.loshra img').offsetWidth;
+    loshara.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSider();
+}
+
+init();
+document.querySelector('.SmehSed').addEventListener('click',function(){
+ count++;
+ if(count >= images.length ){
+    count = 0 ;
+ }
+ rollSider();
+})
+document.querySelector('.SmehPre').addEventListener('click',function(){
+ count--;
+ if(count < 0){
+    count = images.length -1 ;
+ }
+ rollSider();
+})
+function rollSider(){
+    loshara.style.transform = 'translateX(-'+ count * width + 'px';
+}
